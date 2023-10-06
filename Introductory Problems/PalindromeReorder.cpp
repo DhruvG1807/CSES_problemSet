d4511b47dbb4ccff350f631950757b6693e13aeb
Dhruv Gupta <91288069+DhruvG1807@users.noreply.github.com>
2023-10-06 19:39:33 +0530

    Create PalindromeReorder.cpp

---
+++ Introductory Problems/PalindromeReorder.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define set_bits(x) __builtin_popcountll(x)
+#define all(x) (x).begin(), (x).end()
+#define nline "\n"
+ 
+#ifdef dhruv1807
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+void solve(){
+    string s;
+    cin >> s;
+    int n = s.size();
+    map<char,int>d;
+    for (auto i: s){
+        if (d.find(i) != d.end()) d[i]++;
+        else d[i] = 1;
+    }
+ 
+    bool var = false;
+    if (n%2 != 0) var = true;
+    vector<char>ans(n);
+    int i = 0;
+ 
+    for(auto itr: d){
+        if (itr.second%2 > 0){
+            if (var){
+                var = false;
+                ans[n/2] = itr.first;
+                itr.second--;
+                for (int j=0; j<itr.second; j=j+2){
+                    ans[i++] = itr.first;
+                    ans[n-i] = itr.first;
+                }
+            }
+            else{
+                cout<<"NO SOLUTION"<<nline;
+                return;
+            }
+        }
+        else{
+            for (int j=0; j<itr.second; j=j+2){
+                ans[i++] = itr.first;
+                ans[n-i] = itr.first;
+            }
+        }
+    }
+ 
+    for (auto i:ans) cout<<i;
+    cout<<nline;
+}
+ 
+int main(){
+    fastio();
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r", stdin);
+        freopen("output.txt","w",stdout);
+        freopen("error.txt","w",stderr);
+    #endif
+ 
+    ll t=1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+ 
+    return 0;
+}
